7c476794cab6670f2715a0d3031cd07a066518c2
Pritam-Git01 <pritamyadav.ds.work.@gmail.com>
2023-05-29 12:41:40 +0530

    added all library

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -1,17 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { store } from './App/store'
 import { Provider } from 'react-redux'
 
 function App() {
   return (
-    ReactDOM.render(
+   <div className='App'>
       <Provider store={store}>
         
-      </Provider>,
-      document.getElementById('root')
+      </Provider>
+      </div>
     )
-  );
+ 
 }
 
 export default App;
